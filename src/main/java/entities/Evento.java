428ec53e574cbd3a4010379551fe34ca09497b66
Andrea Di Paola <andr3a.dipaola@gmail.com>
2023-05-17 09:08:57 +0200

    first commit

---
+++ src/main/java/entities/Evento.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Evento {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String titolo;
+	private LocalDate dataEvento;
+	private String descrizione;
+	@Enumerated(EnumType.STRING)
+	private TipoEvento tipoEvento;
+	private int numeroMassimoPartecipanti;
+}
